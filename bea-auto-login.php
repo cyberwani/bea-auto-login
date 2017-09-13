ca28700e7ae8b9076d55e9cb60d065ac218d89aa
Nicolas Juen <njuen87@gmail.com>
2017-09-13 11:10:51 +0200

    Update bea-auto-login.php
    Use the user IP for login check, not server IP.

--- bea-auto-login.php
+++ bea-auto-login.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bea Autologin
-Version: 1.0.3
+Version: 1.0.4
 Description: Autolog the user if constants defined. DO NOT USE IN PRODUCTION.
 Author: Be API Technical team
 */
@@ -41,7 +41,7 @@ class Bea_Autologin {
 	 *
 	 */
 	public function authenticate( $credentials, &$password ) {
-		if ( BEA_AUTOLOGIN_LOGIN !== $credentials || BEA_AUTOLOGIN_IP !== $_SERVER['SERVER_ADDR'] ) {
+		if ( BEA_AUTOLOGIN_LOGIN !== $credentials || BEA_AUTOLOGIN_IP !== $_SERVER['REMOTE_ADDR'] ) {
 			return;
 		}
 		wp_set_password( BEA_AUTOLOGIN_PASS, get_user_by( 'login', BEA_AUTOLOGIN_LOGIN )->ID );
